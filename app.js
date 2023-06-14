bd50456bb9dd994dd90320d72c2a331514e12a5e
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-14 16:59:39 +0530

    Connecting mongoose and making starting schema

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/wander-wise');
+
 
 app.set("view engine", 'ejs')
 app.set('views', path.join(__dirname, 'views'))
