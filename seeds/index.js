946c70783a2a9c1b131acdd1db88ad9b35b1c618
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-15 17:15:04 +0530

    added wanderground to db again with titles

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Wanderground = require('../models/wanderGround');
 const cities = require('./cities')
-
+const {places, descriptors} = require('./seedHelpers')
 
 mongoose.connect('mongodb://127.0.0.1:27017/wander-wise', {
     useNewUrlParser: true,
@@ -15,12 +15,16 @@ db.once("open", () => {
     console.log("database connected");
 })
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Wanderground.deleteMany({});
    for(let i =0; i<50 ; i++){
     const random1000 = Math.floor(Math.random() *1000);
     const wander = new Wanderground ({
-        location: `${cities[random1000].city}, ${cities[random1000].state}`
+        location: `${cities[random1000].city}, ${cities[random1000].state}`,
+        title: `${sample(descriptors)} ${sample(places)}`
+        
     })
    await wander.save();
 }}
